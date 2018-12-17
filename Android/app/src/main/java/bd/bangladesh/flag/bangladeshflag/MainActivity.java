8c6251c92ea974868dadec4b9964c0a42f41b62c
Pritom Saha <pritomkumarsaha@live.com>
2018-12-18 02:34:28 +0600

    Bangladesh flag in android
    - minimum android version - 19

---
+++ Android/app/src/main/java/bd/bangladesh/flag/bangladeshflag/MainActivity.java
@@ -0,0 +1,20 @@
+package bd.bangladesh.flag.bangladeshflag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+}
